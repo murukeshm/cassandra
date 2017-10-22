8dd6ac6c81508744877a325af9e26bb0127f93e6
Murukesh Mohanan <murukesh.mohanan@gmail.com>
2017-10-22 23:19:51 +0530

    fix gen.sh to only run on tags with make html

--- gen.sh
+++ gen.sh
@@ -1,32 +1,44 @@
 #! /bin/bash
-git tag -l |
-	grep -xP 'cassandra-\d\.\d+(.\d+)?' |
-	sort -rV |
+trap 'git checkout gh-pages; git commit -a --amend --no-edit' EXIT
+git commit --allow-empty -m "Update docs ($(date))"
+gen_docs () (
+	set -e
+	git checkout "$1"
+	make -C doc html
+	git checkout gh-pages
+	v="${1#*-}"
+	rm -rf "$v"
+	mv doc/build/html "$v"
+	rm -r doc
+	git add "$v"
+	git commit -a --amend --no-edit
+)
+
+# Get all tags containing the commit that added `make html`
+git tag -l 'cassandra-*' --sort=v:refname --contains cad277be41ad4d5488cb96c5ef3f2fcb1cc8d3d9 |
 	while read version
 	do
-		git checkout "$version"
-		(
-			set -e
-			cd doc
-			make SPHINXBUILD=sphinx-build2 html
-			mv build/html ../${version#*-}
-		)
+		gen_docs "$version"
 	done
+gen_docs trunk
 
+versions=(trunk [0-9].*)
 {
-	printf '%s\n' '<select onChange="window.location.pathname=window.location.pathname.replace(/[0-9.]+/,this.value)">'
-	for i in [0-9].*
+	printf '%s\n' '<select onChange="window.location.pathname=window.location.pathname.replace(/[0-9.]+|trunk/,this.value)">'
+	for i in  "${versions[@]}"
 	do
 		printf '    <option value="%s">%s</option>\n' "$i" "$i"
-	done | sort -V
+	done | sort -rV
 	printf '%s\n' '</select>'
 }> versions.html
 
-grep Copyright [0-9].* --include='*.html' -Rl | xargs sed -i '/Copyright/r versions.html'
-for i in [0-9].*
+grep Copyright "${versions[@]}" --include='*.html' -Rl | xargs sed -i '/Copyright/r versions.html'
+for i in "${versions[@]}"
 do
 	(
 		cd "$i"
 		grep Copyright . --include='*.html' -Rl | xargs sed -i "/option.*$i/s/option/& selected='selected'/"
 	)
 done
+rm -r doc versions.html
+git commit -a --amend --no-edit
